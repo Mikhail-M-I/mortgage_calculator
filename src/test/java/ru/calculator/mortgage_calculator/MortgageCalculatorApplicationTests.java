695ce4a5f8890f6ff0ca5ddecc9a72860c2e0e55
murashkovskii <murashkovsky@isands.ru>
2021-06-06 14:48:01 +0700

    Hello World Spring

---
+++ src/test/java/ru/calculator/mortgage_calculator/MortgageCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.calculator.mortgage_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
